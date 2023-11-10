3a9bce660104a7b9a7071e5158faa3d3a5c6d624
titus <80385479+titusio@users.noreply.github.com>
2023-11-10 15:36:51 +0100

    Make `VoxelWorld` export property of `VoxelWorldInstance`

--- src/voxel_world_instance.cpp
+++ src/voxel_world_instance.cpp
@@ -12,4 +12,18 @@ VoxelWorldInstance::~VoxelWorldInstance()
 
 void VoxelWorldInstance::_bind_methods()
 {
+    ClassDB::bind_method(D_METHOD("get_voxel_world"), &VoxelWorldInstance::get_voxel_world);
+    ClassDB::bind_method(D_METHOD("set_voxel_world", "p_world"), &VoxelWorldInstance::set_voxel_world);
+
+    ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "voxel_world", PropertyHint::PROPERTY_HINT_RESOURCE_TYPE, "VoxelWorld"), "set_voxel_world", "get_voxel_world");
+}
+
+Ref<VoxelWorld> VoxelWorldInstance::get_voxel_world() const
+{
+    return world;
+}
+
+void VoxelWorldInstance::set_voxel_world(Ref<VoxelWorld> p_world)
+{
+    world = p_world;
 }
--- src/voxel_world_instance.hpp
+++ src/voxel_world_instance.hpp
@@ -2,6 +2,7 @@
 #define VOXEL_WORLD_INSTANCE_HPP
 
 #include <godot_cpp/classes/node3d.hpp>
+#include "voxel_world.hpp"
 
 namespace godot
 {
@@ -10,8 +11,14 @@ namespace godot
         GDCLASS(VoxelWorldInstance, Node3D)
 
     public:
+        Ref<VoxelWorld> world;
+
+    public:
         VoxelWorldInstance();
         ~VoxelWorldInstance();
+
+        Ref<VoxelWorld> get_voxel_world() const;
+        void set_voxel_world(Ref<VoxelWorld> p_world);
 
     protected:
         static void _bind_methods();
