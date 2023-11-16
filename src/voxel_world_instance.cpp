fae8bc05d23020eb022051875c6beadc3c926f71
titus <80385479+titusio@users.noreply.github.com>
2023-11-16 07:08:16 +0100

    Add Generation Base

--- src/voxel_world_instance.cpp
+++ src/voxel_world_instance.cpp
@@ -1,6 +1,10 @@
 #include "voxel_world_instance.hpp"
 
 #include <godot_cpp/variant/utility_functions.hpp>
+#include <godot_cpp/variant/packed_vector3_array.hpp>
+#include <godot_cpp/variant/packed_int32_array.hpp>
+#include <godot_cpp/classes/array_mesh.hpp>
+#include <godot_cpp/variant/array.hpp>
 
 using namespace godot;
 
@@ -32,10 +36,165 @@ void VoxelWorldInstance::set_voxel_world(Ref<VoxelWorld> p_world)
 
 void VoxelWorldInstance::generate()
 {
-    UtilityFunctions::print("VoxelWorldInstance::generate()");
+    UtilityFunctions::print("start generating");
+    const int CHUNK_SIZE = 16;
+    const float voxel_size = 0.1f;
+    const Color color = Color("#CCD1D1");
+
+    PackedVector3Array vertices = PackedVector3Array();
+    PackedVector3Array normals = PackedVector3Array();
+    PackedColorArray colors = PackedColorArray();
+    PackedInt32Array indices = PackedInt32Array();
+
+    const Vector3 a = Vector3(0.0, 1.0, 0.0);
+    const Vector3 b = Vector3(1.0, 1.0, 0.0);
+    const Vector3 c = Vector3(1.0, 0.0, 0.0);
+    const Vector3 d = Vector3(0.0, 0.0, 0.0);
+    const Vector3 e = Vector3(0.0, 1.0, 1.0);
+    const Vector3 f = Vector3(1.0, 1.0, 1.0);
+    const Vector3 g = Vector3(1.0, 0.0, 1.0);
+    const Vector3 h = Vector3(0.0, 0.0, 1.0);
+
+    const Vector3 north = Vector3(0.0, 0.0, -1.0);
+    const Vector3 south = Vector3(0.0, 0.0, 1.0);
+    const Vector3 top = Vector3(0.0, 1.0, 0.0);
+    const Vector3 bottom = Vector3(0.0, -1.0, 0.0);
+    const Vector3 east = Vector3(1.0, 0.0, 0.0);
+    const Vector3 west = Vector3(-1.0, 0.0, 0.0);
+
+    int index = 0;
+
+    for (int x = 0; x < CHUNK_SIZE; x++)
+    {
+        for (int y = 0; y < CHUNK_SIZE; y++)
+        {
+            for (int z = 0; z < CHUNK_SIZE; z++)
+            {
+                Vector3 position = Vector3(x, y, z) * voxel_size;
+
+                // North Face
+                vertices.append(a); // 0
+                vertices.append(b); // 1
+                vertices.append(f); // 2
+                vertices.append(e); // 3
+                indices.append(index + 0);
+                indices.append(index + 1);
+                indices.append(index + 2);
+                indices.append(index + 0);
+                indices.append(index + 2);
+                indices.append(index + 3);
+                index += 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    normals.append(north);
+                    colors.append(color);
+                }
+
+                // south face
+                vertices.append(e); // 0
+                vertices.append(f); // 1
+                vertices.append(g); // 2
+                vertices.append(h); // 3
+                indices.append(index + 0);
+                indices.append(index + 1);
+                indices.append(index + 2);
+                indices.append(index + 0);
+                indices.append(index + 2);
+                indices.append(index + 3);
+                index += 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    normals.append(south);
+                    colors.append(color);
+                }
+
+                // top
+                vertices.append(a); // 0
+                vertices.append(b); // 1
+                vertices.append(f); // 2
+                vertices.append(e); // 3
+                indices.append(index + 0);
+                indices.append(index + 1);
+                indices.append(index + 2);
+                indices.append(index + 0);
+                indices.append(index + 2);
+                indices.append(index + 3);
+                index += 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    normals.append(top);
+                    colors.append(color);
+                }
+
+                // bottom
+                vertices.append(h); // 0
+                vertices.append(g); // 1
+                vertices.append(c); // 2
+                vertices.append(d); // 3
+                indices.append(index + 0);
+                indices.append(index + 1);
+                indices.append(index + 2);
+                indices.append(index + 0);
+                indices.append(index + 2);
+                indices.append(index + 3);
+                index += 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    normals.append(bottom);
+                    colors.append(color);
+                }
+
+                // west
+                vertices.append(a); // 0
+                vertices.append(e); // 1
+                vertices.append(h); // 2
+                vertices.append(d); // 3
+                indices.append(index + 0);
+                indices.append(index + 1);
+                indices.append(index + 2);
+                indices.append(index + 0);
+                indices.append(index + 2);
+                indices.append(index + 3);
+                index += 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    normals.append(west);
+                    colors.append(color);
+                }
+
+                // east
+                vertices.append(f); // 0
+                vertices.append(b); // 1
+                vertices.append(c); // 2
+                vertices.append(g); // 3
+                indices.append(index + 0);
+                indices.append(index + 1);
+                indices.append(index + 2);
+                indices.append(index + 0);
+                indices.append(index + 2);
+                indices.append(index + 3);
+                index += 4;
+                for (int i = 0; i < 4; i++)
+                {
+                    normals.append(east);
+                    colors.append(color);
+                }
+            }
+        }
+    }
+
+    Ref<ArrayMesh> mesh = memnew(ArrayMesh);
+    Array surface_arrays = Array();
+    surface_arrays.resize(Mesh::ARRAY_MAX);
+    surface_arrays[Mesh::ARRAY_VERTEX] = vertices;
+    surface_arrays[Mesh::ARRAY_INDEX] = indices;
+    surface_arrays[Mesh::ARRAY_NORMAL] = normals;
+    surface_arrays[Mesh::ARRAY_COLOR] = colors;
+    mesh->add_surface_from_arrays(Mesh::PRIMITIVE_TRIANGLES, surface_arrays);
+    UtilityFunctions::print(index);
+    UtilityFunctions::print("end generating");
 }
 
 void VoxelWorldInstance::clear()
 {
-    UtilityFunctions::print("VoxelWorldInstance::clear()");
 }
--- src/voxel_world_instance.hpp
+++ src/voxel_world_instance.hpp
@@ -6,6 +6,24 @@
 
 namespace godot
 {
+    //               010           110                         Y
+    //   Vertices     A0 ---------- B1            Faces      Top    -Z
+    //           011 /  |      111 /  |                        |   North
+    //             E4 ---------- F5   |                        | /
+    //             |    |        |    |          -X West ----- 0 ----- East X
+    //             |   D3 -------|-- C2                      / |
+    //             |  /  000     |  / 100               South  |
+    //             H7 ---------- G6                      Z    Bottom
+    //              001           101                          -Y
+    //
+    // Vertices
+    // b, a, d, b, d, c, -> N
+    // e, f, g, e, g, h, -> S
+    // a, e, h, a, h, d, -> W
+    // f, b, c, f, c, g, -> E
+    // a, b, f, a, f, e, -> T
+    // h, g, c, h, c, d, -> B
+
     class VoxelWorldInstance : public Node3D
     {
         GDCLASS(VoxelWorldInstance, Node3D)
