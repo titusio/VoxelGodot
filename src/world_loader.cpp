6184971f80a18be01d1c2f8d5848949bf39c43ea
titus <80385479+titusio@users.noreply.github.com>
2023-12-11 07:39:20 +0100

    add load and unload methods to WorldLoader

--- src/world_loader.cpp
+++ src/world_loader.cpp
@@ -4,10 +4,27 @@ using namespace godot;
 
 WorldLoader::WorldLoader()
 {
+	loaded_chunks = Array();
 }
 
 WorldLoader::~WorldLoader()
 {
+}
+
+void WorldLoader::load_chunk(Vector3i p_position)
+{
+	ERR_FAIL_NULL_MSG(world_instance, "WorldLoader::load_chunk: world_instance is null!");
+
+	loaded_chunks.append(p_position);
+	world_instance->load_chunk(p_position);
+}
+
+void WorldLoader::unload_chunk(Vector3i p_position)
+{
+	ERR_FAIL_NULL_MSG(world_instance, "WorldLoader::unload_chunk: world_instance is null!");
+
+	loaded_chunks.erase(p_position);
+	world_instance->unload_chunk(p_position);
 }
 
 VoxelWorldInstance* WorldLoader::get_world_instance() const
@@ -25,4 +42,7 @@ void WorldLoader::_bind_methods()
 	ClassDB::bind_method(D_METHOD("get_world_instance"), &WorldLoader::get_world_instance);
 	ClassDB::bind_method(D_METHOD("set_world_instance", "world_instance"), &WorldLoader::set_world_instance);
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "world_instance", PROPERTY_HINT_NODE_TYPE, "VoxelWorldInstance"), "set_world_instance", "get_world_instance");
+
+	ClassDB::bind_method(D_METHOD("load_chunk", "p_position"), &WorldLoader::load_chunk);
+	ClassDB::bind_method(D_METHOD("unload_chunk", "p_position"), &WorldLoader::unload_chunk);
 }
--- src/world_loader.hpp
+++ src/world_loader.hpp
@@ -2,6 +2,7 @@
 #define CHUNK_LOADER_HPP
 
 #include <godot_cpp/classes/node3d.hpp>
+#include <godot_cpp/variant/builtin_binds.hpp>
 
 #include "voxel_world_instance.hpp"
 
@@ -13,10 +14,14 @@ namespace godot
 
 	private:	
 		VoxelWorldInstance* world_instance;
+		Array loaded_chunks;
 
 	public:
 		WorldLoader();
 		~WorldLoader();
+
+		void load_chunk(Vector3i p_position);
+		void unload_chunk(Vector3i p_position);
 
 		VoxelWorldInstance* get_world_instance() const;
 		void set_world_instance(VoxelWorldInstance* world_instance);
